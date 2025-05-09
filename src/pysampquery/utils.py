7c1020f354d8c04e66616fd116d7e67ef9ef91b3
Leo Araya <larayavrs@gmail.com>
2025-05-08 21:16:18 -0400

    feat: create utils module for utility functions

---
+++ src/pysampquery/utils.py
@@ -0,0 +1,10 @@
+"""
+This module is used for utility functions used by the library in another modules
+"""
+
+from __future__ import annotations
+
+import struct
+import cchardet as chdet
+
+# TODO: add more utils
