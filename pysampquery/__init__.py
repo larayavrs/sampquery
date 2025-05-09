51c5ffd6c3ab45c67e40a7c17c3b0a83dce778b1
Leo Araya <larayavrs@gmail.com>
2025-05-09 16:24:13 -0400

    feat: implement core classes and utilities for PySAMPQuery library

--- pysampquery/__init__.py
+++ pysampquery/__init__.py
@@ -1,6 +1,10 @@
 """pysampquery ― better GTA SA:MP query client."""
 
+from .client import PySAMPQuery_Client
+
 __name__ = "pysampquery"
 __version__ = "0.0.1"
 __author__ = "larayavrs"
 __email__ = "larayavrs@gmail.com"
+
+__all__ = ["PySAMPQuery_Client"]
