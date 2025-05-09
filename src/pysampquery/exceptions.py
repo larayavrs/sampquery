a3f53b32f6b30ec10ef3cdbdbfd3640f85b0a45c
Leo Araya <larayavrs@gmail.com>
2025-05-08 21:16:05 -0400

    feat: add exception handling classes for RCON and port validation

---
+++ src/pysampquery/exceptions.py
@@ -0,0 +1,21 @@
+"""
+In this module we handle exceptions raised by the library
+"""
+
+from __future__ import annotations
+
+class PySAMPQuery_MissingRCON(Exception):
+    """Raised when RCON password is missing"""
+    pass
+
+class PySAMPQuery_InvalidRCON(Exception):
+    """Raised when RCON password is invalid"""
+    pass
+
+class PySAMPQuery_DisabledRCON(Exception):
+    """Raised when RCON is disabled, the server is not using RCON"""
+    pass
+
+class PySAMPQuery_InvalidPort(Exception):
+    """Raised when port is invalid"""
+    pass
