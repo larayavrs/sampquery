fde0a413710b336ef2f6e9896572a2fd35b26495
Leo Araya <larayavrs@gmail.com>
2025-05-09 17:04:35 -0400

    feat: update example script to use Trio for asynchronous execution

--- example.py
+++ example.py
@@ -1,18 +1,16 @@
-import asyncio
+from trio import run
 from pysampquery import PySAMPQuery_Client
 
-
 async def main():
-    latam = PySAMPQuery_Client("154.38.184.18", 7777)
-
+    latam = PySAMPQuery_Client(
+        ip='144.217.174.214',
+        port=6969,
+        rcon_password=None
+    )
     server_info = await latam.info()
     print(f"Server name: {server_info.name}")
-
     players_list = await latam.players()
-    print(f"Players:")
-
     for player in players_list.players:
         print(f"> {player.name}")
 
-
-asyncio.run(main())
+run(main)
