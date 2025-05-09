c3164203a61c406da76b87a15e21d3fc4e09b25c
Leo Araya <larayavrs@gmail.com>
2025-05-09 13:39:21 -0400

    feat: add PySAMPQuery_RuleList class to manage server rules

--- src/pysampquery/rule.py
+++ src/pysampquery/rule.py
@@ -2,6 +2,8 @@
 In this module we storage the rules of the server
 """
 
+import struct
+
 from __future__ import annotations
 from dataclasses import dataclass
 from .utils import PySAMPQuery_Utils
@@ -32,3 +34,31 @@ class PySAMPQuery_Rule:
         name, data, _ = PySAMPQuery_Utils.unpack_string(data, "B")
         value, data, encoding = PySAMPQuery_Utils.unpack_string(data, "B")
         return cls(name=name, value=value, encoding=encoding), data
+
+
+@dataclass
+class PySAMPQuery_RuleList:
+    """
+    Represents a list of the server rules
+
+    :param list[PySAMPQuery_Rule] rules: The list of rules
+    """
+
+    rules: list[PySAMPQuery_Rule]
+
+    @classmethod
+    def from_data(cls, data: bytes) -> PySAMPQuery_RuleList:
+        """
+        Creates an instance of PySAMPQuery_RuleList from raw data
+
+        :param bytes data: The raw data to parse into rule list information
+        :return PySAMPQuery_RuleList: An instance of PySAMPQuery_RuleList with the parsed data
+        """
+        rcount = struct.unpack_from("<H", data)[0]
+        data = data[2:]
+        rules = []
+        for _ in range(rcount):
+            rule, data = PySAMPQuery_Rule.from_data(data)
+            rules.append(rule)
+        assert not data
+        return cls(rules=rules)
